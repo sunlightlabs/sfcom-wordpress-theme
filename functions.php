1f78dc81b4b152f440755141f611ee3accb749a6
Bill Hunt <bill@krues8dr.com>
2016-10-28 14:16:47 -0400

    Bump version

--- functions.php
+++ functions.php
@@ -94,7 +94,7 @@ function html5blank_conditional_scripts()
 // Load HTML5 Blank styles
 function html5blank_styles()
 {
-    wp_register_style('html5blank', get_template_directory_uri() . '/static/css/main.css', array(), '1.0.6', 'all');
+    wp_register_style('html5blank', get_template_directory_uri() . '/static/css/main.css', array(), '1.0.7', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
 }
 
